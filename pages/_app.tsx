750a395200db157fb4126b565874f9f5913cd730
lowkasen <lowkasen@gmail.com>
2022-02-26 14:30:59 +0800

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Web3 from "web3";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const web3 = new Web3("ws://localhost:8546");
+    console.log(typeof web3);
+    console.log(web3.currentProvider);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
